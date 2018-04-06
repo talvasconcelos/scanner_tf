003ad06cc2bbbdc8f36d56d422324219945db65f
Tiago Prosp <tiago@prosp>
2018-04-06 16:33:12 +0100

    ...

--- index.js
+++ index.js
@@ -20,11 +20,11 @@ function create_dataSet(data, ip = INDICATOR_PERIOD, wl = WINDOW_LEN) {
       let _MFI = ind.MFI.slice(j, i)//.map(v => v)
       let _RSI = ind.RSI.slice(j, i)//.map(v => v)
       let price = ind.price_change.slice(j, i)
-      X.push(tf.tensor3d([
+      X.push([
         price[0], _MFI[0], _RSI[0],
         price[1], _MFI[1], _RSI[1],
         price[2], _MFI[2], _RSI[2],
-        price[3], _MFI[3], _RSI[3]]), [1, 4, 3])
+        price[3], _MFI[3], _RSI[3]])
       Y.push(tf.tensor([ind.output[i]]))
       // set.push([
       //   price[0], _MFI[0], _RSI[0], price[1], _MFI[1], _RSI[1], price[2], _MFI[2], _RSI[2], price[3], _MFI[3], _RSI[3], ind.output[i]
@@ -32,13 +32,16 @@ function create_dataSet(data, ip = INDICATOR_PERIOD, wl = WINDOW_LEN) {
     }
   })
   return [X, Y]
-  //fs.writeFileSync('data.json', JSON.stringify(set), err => console.log(err))
-  //return set
+  // return set
 }
-
-
+// let t = create_dataSet(test)
+// fs.writeFileSync('test.json', JSON.stringify(t), err => console.log(err))
+let model
 let [xTrain, yTrain] = create_dataSet(data)
-// xTrain = xTrain.map(v => tf.tensor3d(v, [1, 4, 3]))
+let [xTest, yTest] = create_dataSet(test)
+xTrain = xTrain.map(v => tf.tensor3d(v, [1, 4, 3]))
+xTest = xTest.map(v => tf.tensor3d(v, [1, 4, 3]))
+//xTrain[0].print()
 //xTrain = tf.tensor(xTrain)
 //yTrain = tf.tensor(yTrain)
 //yTrain = yTrain.map(v => tf.scalar(v))
@@ -48,49 +51,49 @@ let [xTrain, yTrain] = create_dataSet(data)
 // const xTrain = create_dataSet(data).map(cur => cur.input)
 // const yTrain = create_dataSet(data).map(cur => cur.output)
 
+async function execute() {
+  const optimizer = tf.train.adam(0.01)
+  model = tf.sequential()
 
-const model = tf.sequential()
+  model.add(tf.layers.lstm({units: 12, returnSequences: true, inputShape: [4, 3]}))
+  model.add(tf.layers.dropout({rate: 0.2}))
 
-// model.add(tf.layers.simpleRNN({units: 10, returnSequences: true, inputShape: [2981, 3]}))
-// model.add(tf.layers.dense({units: 1, activation: 'sigmoid'}))
-// model.compile({
-//   optimizer,
-//   loss: 'categoricalCrossentropy',
-//   metrics: ['accuracy']
-// })
-// for (var i = 0; i < /*xTrain.length*/2; i++) {
-//   const h = model.fit(xTrain[i], yTrain[i], {
-//     batchSize: 10,
-//     epochs: 50
-//   }).catch(e => console.error(e.stack))
-//   //console.log(h.history.loss[0]);
-// }
-const optimizer = tf.train.adam(0.01)
+  model.add(tf.layers.flatten())
 
+  model.add(tf.layers.dense({units: 1, activation: 'sigmoid'}))
 
-model.add(tf.layers.lstm({units: 8, returnSequences: true, inputShape: [4, 3]}))
-model.add(tf.layers.dropout({rate: 0.2}))
+  model.compile({
+    optimizer,
+    loss: 'binaryCrossentropy',
+    metrics: ['accuracy']
+  })
 
-//model.add(tf.layers.lstm({units: 2, returnSequences: true}))
-
-model.add(tf.layers.flatten())
-
-model.add(tf.layers.dense({units: 1, activation: 'softmax'}))
-model.compile({
-  optimizer,
-  loss: 'binaryCrossentropy',
-  metrics: ['accuracy']
-})
-
-for (var i = 0; i < xTrain.length; i++) {
-  model.fit(xTrain[i], yTrain[i], {
-    batchSize: 1,
-    epochs: 3
-  }).then(() => {
-    console.log(model.history.loss[0]);
-  }).catch(e => console.error(e.stack))
+  for (var i = 0; i < xTrain.length; i++) {
+    const h = await model.fit(xTrain[i], yTrain[i], {
+      batchSize: 16,
+      epochs: 3
+    }).catch(e => console.error(e))
+    i % 100 == 0 ? console.log(h.history.loss[0]) : null
+  }
 
 }
+
+execute()
+for (var i = 0; i < xTest.length; i++) {
+  model.evaluate(xTest[i], yTest[i], {verbose: 0})
+}
+
+
+
+// for (var i = 0; i < xTrain.length; i++) {
+//   model.fit(xTrain[i], yTrain[i], {
+//     batchSize: 1,
+//     epochs: 3
+//   }).then(() => {
+//     console.log(model.history.loss[0]);
+//   }).catch(e => console.error(e.stack))
+//
+// }
 
 // model.fit(xTrain[0], yTrain[0], {
 //   batchSize: 10,
